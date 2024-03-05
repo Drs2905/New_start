5ec5513eebe43d6cd24315f9b7da473732e00f6a
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-05 19:34:31 +0530

    Add files via upload

---
+++ new start/java/typecast2.java
@@ -0,0 +1,17 @@
+public class typecast2 {
+    public static void main(String[] args) {
+        // char a='a';
+        // char b='b';
+        // System.out.println((int)(b));
+        // System.out.println((int)(a));
+        // System.out.println(b-a);
+
+        short a=5;
+        byte b=25;
+        char c='c';
+        byte bt=(byte)(a+b+c);
+        System.out.println(bt);
+
+
+    }
+}
