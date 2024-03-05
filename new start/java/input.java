6cc92e297221791b6251dbc8f421ea2e568af9c8
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-05 13:23:04 +0530

    Add files via upload

---
+++ new start/java/input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class input {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        // String input=sc.next();
+        // System.out.println(input);
+
+        //for taking input for entire line we use below  method
+        String name=sc.nextLine();
+        System.out.println(name);
+
+        //taking input for numbers we use below  methods
+        int number=sc.nextInt();
+        System.out.println(number);
+
+
+    }
+}
