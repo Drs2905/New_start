42089c9a4fffa30e23325f25fc271064b1ca64e4
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-06 22:06:59 +0530

    Add files via upload

---
+++ new start/java/operatos.java
@@ -0,0 +1,16 @@
+public class operatos {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        int sub=a-b;
+        int multi=a*b;
+        int divi=a/b;
+        int modul=a%b;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(divi);
+        System.out.println(modul);
+    }
+}
