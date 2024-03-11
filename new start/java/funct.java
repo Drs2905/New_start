3f7595ba9140f2bcd838924c3e2f9f9ff442d04e
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-11 15:51:47 +0530

    Add files via upload

---
+++ new start/java/funct.java
@@ -0,0 +1,9 @@
+public class funct{
+    public static void printHelloworld(){
+        System.out.println( "Hello World!" );
+        return;
+    }
+    public static void main(String[] args) {
+         printHelloworld();
+    }
+}
