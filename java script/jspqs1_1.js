b398b2926158d16bfb4d2b486c6895f309f54f3d
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-12 19:10:11 +0530

    Add files via upload

---
+++ java script/jspqs1_1.js
@@ -0,0 +1,10 @@
+let color="red";
+if(color==="red"){
+    console.log("stop");
+}
+if(color==="yellow"){
+    console.log("go");
+}
+if(color==="yellow"){
+    console.log("slowdown");
+}
---
+++ java script/jspqs1_2.js
@@ -0,0 +1,13 @@
+let size="s";
+if(size=="xl"){
+    console.log("price is 250");
+}
+else if(size=="l"){
+    console.log("price is 200");
+}
+else if(size=="m"){
+    console.log("price is 100");
+}
+else{
+    console.log("price is 50");
+}
