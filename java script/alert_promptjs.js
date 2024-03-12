1d41950b1665c8d57276f1e33982743a7ee370f6
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-12 21:05:14 +0530

    Add files via upload

---
+++ java script/alert_promptjs.js
@@ -0,0 +1,13 @@
+// alert("Danger go back");
+// console.error("this is error message");
+// console.warn("this is a warning message");
+//prompt("enter your name");
+
+// let firstName=prompt("enter your name :");
+// console.log(firstName);
+
+let firstName=prompt("enter first name :");
+let lastName=prompt("enter last name :");
+//console.log(firstName, " ", lastName);
+let msg=firstName+" "+ lastName;
+alert(msg)
---
+++ java script/switchjs.js
@@ -0,0 +1,14 @@
+let color="blue";
+switch(color){
+    case"red" :
+    console.log("stop");
+    break;
+    case"green" :
+    console.log("go");
+    break;
+    case"yellow" :
+    console.log("slowdown");
+    break;
+    default:
+        console.log("light is broken");
+}
