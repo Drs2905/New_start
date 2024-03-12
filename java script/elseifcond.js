c0946a2c0b9fca6305fb19c0c42278c4f3aa638f
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-12 19:09:43 +0530

    Add files via upload

---
+++ java script/elseifcond.js
@@ -0,0 +1,41 @@
+//let age=40;
+let age=14;
+if(age>=18){
+    console.log("You are eligible to vote");
+}
+else if(age<18){
+    console.log("Sorry, you are not eligible for voting yet")
+}
+
+let marks=45;
+if(marks>=80){
+    console.log("A+");
+}
+else if(marks>=60){
+    console.log("a");
+}
+else if(marks>=33){
+    console.log("b");
+}
+
+let num=18;
+if(num>=18){
+    console.log("you can vote");
+}
+else{
+    console.log("you can vote");
+}
+
+let color="blue";
+if(color=="red"){
+    console.log("stop");
+}
+else if(color=="green"){
+    console.log("go");
+}
+else if(color=="yellow"){
+    console.log("slowdown");
+}
+else{
+    console.log("traffic light is broken");
+}
