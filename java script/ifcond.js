0a4cd27cda282f04d9fae601b8b02a8df78a8a73
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-12 18:32:20 +0530

    Add files via upload

---
+++ java script/ifcond.js
@@ -0,0 +1,14 @@
+console.log("befor my if statement");
+    //let age=23;
+    let age=13;
+    if(age>=18){
+        console.log("you can vote");
+        console.log("you can drive");
+        let a=5;
+        console.log(5**2);
+    }
+    if(age<18){
+        console.log("you cant vote");
+
+    }
+console.log("after my if statement");
