99a956d42de27f3c13bf4915296216e038adeec7
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-12 18:31:24 +0530

    Add files via upload

---
+++ java script/app2.js
@@ -0,0 +1,5 @@
+let pencilPrice=10;
+let erasorPrice=5;
+//let output="the total price is :"+(pencilPrice+erasorPrice)+ "rupees.";
+let output=`the total price is : ${pencilPrice} rupees.`//back ticks
+console.log(output);
