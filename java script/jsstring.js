b95e868e46e94c3d47823ef1ff05b25214ac3199
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-13 11:36:20 +0530

    Add files via upload

---
+++ java script/jsstring.js
@@ -0,0 +1,20 @@
+// let msg="    hello    "
+// let password=prompt("set your password");
+// console.log(password.trim());
+
+// let name="diptiranjan"
+// console.log(name.toUpperCase());
+
+// let str="Ilovecoding";
+// console.log(str.indexOf("love"));
+
+// let msg="    hello    ";
+// console.log(msg.toUpperCase().trim());
+
+// let str="ilovecoding"
+// console.log(str.slice(1,5));
+// console.log(str.slice(-2));
+
+let msg="ilovecoding";
+// console.log(msg.replace("love","do"));
+console.log(msg.repeat(3));
