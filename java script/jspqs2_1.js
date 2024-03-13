93942d01cfbf7cbfd45b4be7d2fc18ba68209c44
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-13 11:36:00 +0530

    Add files via upload

---
+++ java script/jspqs2_1.js
@@ -0,0 +1,7 @@
+// let msg="   help   ";
+// console.log(msg.toUpperCase().trim());
+
+let name="Appnacollege";
+//console.log(name.slice(4,9));
+//console.log(name.replace("Appna","our"));
+console.log(name.slice(5).replace('l','t'));
