8699d868cf23bee5cb7b07cb8f488e0e4e0c5388
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-13 11:35:41 +0530

    Add files via upload

---
+++ java script/jsasign1_1.js
@@ -0,0 +1,14 @@
+//no 1
+// let num=20;
+// if(num%10==0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+//no 2
+// let name=prompt(  "Please enter your name" );
+// let age=prompt(  "please enter your age" ) ;
+// alert(`${name} is  ${age} years old`);
+
+//no 3
