6ff2a41c5edbea074ba928df66b6f005154b24fc
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 11:18:35 +0530

    Add files via upload

---
+++ java script/jsarray.js
@@ -0,0 +1,17 @@
+// let nums=[2, 4, 6, 8]
+// console.log(nums.length);
+// console.log(typeof nums);
+
+// let marks=[99, 57, 49, 94, 85];
+// let students=[dipti, ranjan, satapathy];
+// let info=["dipti", 25, 8.9];
+// console.log(info);
+
+// let arr=[];
+// console.log(arr);
+
+let name=["rohit", "apple", "litchi"];
+name[0]="banana";
+console.log(name);
+name[10]="papaya";
+console.log(name);
