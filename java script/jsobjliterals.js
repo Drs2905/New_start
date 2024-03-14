732ebaf34ed509c8596450a1d04cb0681da2f39b
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 23:56:07 +0530

    Add files via upload

---
+++ java script/jsobjliterals.js
@@ -0,0 +1,65 @@
+// let student={
+//     name:"drs",
+//     age:23,
+//     marks:94.4,
+// };
+
+// const student={
+//       name:"drs",
+//        age:23,
+//        marks:94.4,
+//        city:"delhi"
+//  op--student.city
+// 'delhi'
+// student.city="mumbai";
+// 'mumbai'
+// student.gender="male"
+// 'male'
+// delete student.marks;
+// true
+ //};
+
+ //create an object litearals for the properties of thread/tweeter post
+//  let properties={
+//     username:"drs_2905",
+//     content: "I love coding!",
+//     likes:100,
+//     reposts:5,
+//     tags:["stsdhjgsd", "gfghjkl"],
+// }; //to get values use properties.likes and properties["content"]
+
+
+//nested objects
+// const classinfo={
+//     aman:{
+//         grade:"a",
+//         city:"delhi",
+//     },
+//     sradha:{
+//         grade:"b",
+//         city:"mumbai",
+//     },
+//     karan:{
+//         grade:"c",
+//         city:"pune",
+//     }
+// };
+
+//array of objects
+const classinfo=[
+  {
+    name:"aman",
+    grade:"a",
+    city:"delhi",
+  },
+  {
+    name:"shradha",
+    grade:"b",
+    city:"pune",
+  },
+  {
+    name:"karan",
+    grade:"c",
+    city:"mumbai",
+  }
+]
