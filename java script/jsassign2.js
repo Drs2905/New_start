4b7baa9ea7168c81e8ab5f77717a5d307f1bcbb0
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 11:18:16 +0530

    Add files via upload

---
+++ java script/jsassign2.js
@@ -0,0 +1,42 @@
+//1.to print first n element
+// let arr=[7,9,0,-2];
+// console.log(arr.length);
+// let n=3;
+// let ans =arr.slice(0,n);
+// console.log(ans);
+
+//2.to print last n element
+// let ans=arr.slice(arr.length-n);
+// console.log(ans);
+
+//3. js program to check whether a string is blank or not
+// let str=prompt("please enter a string");
+// if(str.length==0){
+//     console.log("string is empty");
+// }else{
+
+// }
+
+//4. js program to test whether the character at given index is lower case
+//  let name="Diptiranjan";
+//  let idx=3;
+//  if(name[idx]==name[idx].toLowerCase()){
+//     console.log("character is lowercase");
+//  }else{
+//     console.log("character is uppercase");
+//  }
+
+ //5.Write a JavaScript program to strip leading and trailing spaces from a string.
+    // let str=prompt("Please Enter A String");
+    // console.log(`original string=${str}`);
+    // console.log(`string without spaces=${str.trim()}`);
+
+ //6.Write a JavaScript program to check if an element exists in an array or not
+ let arr=["hello",'a',23,64,99,0];
+ let item=64;
+ if(arr.indexOf(item)!=-1){
+    console.log("element exists in array");
+ }else{
+    console.log("element doesnot exists");
+ }
+
