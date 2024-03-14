d8b2eb99a7d36123ef240f0ac7bd9088d6984d78
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 13:13:06 +0530

    Add files via upload

---
+++ java script/jsloops.js
@@ -0,0 +1,35 @@
+// for(let i=2;i<=5;i++){
+//     console.log(i);
+// }
+//print all odd numbers 1-15
+// for(let i=1;i<=15;i++){
+//     if(i%2 !=0){
+//         console.log(i);
+//     }
+//     else{
+
+//     }
+// }
+
+//print all even nubmers 1-15
+// for(let i=1;i<=15;i++){
+//     if(i%2 ==0){
+//         console.log(i);
+//     }
+//     else{
+
+//     }
+// }
+
+//print multiplication table
+// let n=prompt("write your number");
+// n=parseInt(n);
+// for(let i=n;i<=n*10;i=i+n){
+//     console.log(i);
+// }
+
+for(let i=1;i<=3;i++){
+    for(let j=1;j<=3;j++){
+        console.log(j);
+    }
+}
