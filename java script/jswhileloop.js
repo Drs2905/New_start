17c82fdff6eceb047a50e5caa0747442e9d98be6
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 13:13:28 +0530

    Add files via upload

---
+++ java script/jswhileloop.js
@@ -0,0 +1,30 @@
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=0;
+// while(i<=20){
+//     console.log(i);
+//     i++;
+// }
+
+// let favMovie="avatar";
+// let guess=prompt("enter movie name");
+// while((guess != favMovie) && (guess!="quit")){
+//     console.log("wrong guess")
+//     guess=prompt("wrong guess type again");
+// }
+// console.log("you guess right");
+
+//break keyword
+// let i=1;
+// while(i<=5){
+//     if(i==3){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
