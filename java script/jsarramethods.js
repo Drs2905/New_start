6f836bd5c0591b5bfaff2bba7593e609ca928ae3
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-14 11:18:52 +0530

    Add files via upload

---
+++ java script/jsarramethods.js
@@ -0,0 +1,41 @@
+// let cars=["Saab","Volvo","BMW","maruti"];
+// cars.push("toyota"); //for push method
+// console.log(cars); 
+// cars.pop(); //for pop method
+// console.log(cars);
+// cars.shift("toyota"); //for shift method
+// console.log(cars);
+// cars.unshift(); //for unshift method
+// console.log(cars);
+
+//let months=["jan","feb","march","april"];
+// months.shift();
+// months.shift();
+// months.unshift("june");
+// months.unshift("july");
+// console.log(months);
+
+
+// console.log(months.indexOf( "march" )); //for indexof method
+// console.log(months.includes("feb"));  //for includes method
+//let months2=["may","june","july"];
+// console.log(months.concat(months2));   //for concat mthod
+//console.log(months2.reverse());     //for reverse method
+
+//for slice method
+// console.log(months.slice());  
+// console.log(months.slice(3));
+// console.log(months.slice(1,3));
+// console.log(months.slice(-2));
+
+//for splice method
+//let colors=["red","yellow","blue","orange","pink","white",]; //splice(start,deletecount,item0,....itemN)
+//console.log(colors.splice(4)); //here splice removed pink and white from array cause of splice.
+//console.log(colors.splice(0,1));//here splice will delete red from array. cause starting index is 0 and deletecount index is 1
+//console.log(colors.splice(0,1,"black","grey"));  //here black and grey is added and yellow will
+//console.log(colors);  //here black and grey is added and yellow will
+
+
+//sort in array
+let days=["monday","sunday","friday","wednesday","tuesday"];
+console.log(days.sorat());
