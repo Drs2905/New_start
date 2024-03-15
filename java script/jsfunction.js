08f5099f438a55f296087826cba5dce7e479d24a
Drs2905 <121534069+Drs2905@users.noreply.github.com>
2024-03-15 17:19:38 +0530

    Add files via upload

---
+++ java script/jsfunction.js
@@ -0,0 +1,22 @@
+function hello(){
+    console.log("hello");
+}
+hello();
+
+
+function printName(){
+    console.log("apna college")
+}
+printName();
+
+
+function printPoem(){
+    console.log("twinkle twinkle");
+}
+printPoem();
+
+function rollDice(){
+let random=Math.floor(Math.random()*6)+1;
+console.log(random);
+}
+rollDice();
